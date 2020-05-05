02e0c19ac0f25654195bd03dd91186ef1357dd9b
Wilson MacLeod <wilsonmacleod@gmail.com>
2020-05-05 09:38:19 -0700

    soccer API calls added

---
+++ lackey/actions/sports/nba.py
@@ -0,0 +1,69 @@
+import requests
+import json
+
+import datetime
+import pandas as pd
+
+import NBA_Urls from sports.sport_urls as Urls
+
+"""
+NEED TO FIGURE OUT 
+SEASON RESOLUTION (2019-20)
+"""
+
+today = datetime.datetime.today().strftime('%Y-%m-%d')
+def get_games_for_date(date=today): # default = today
+    url = Urls.scoreboard_url(date)
+    headers = Urls.headers()
+    r = requests.get(url=url, headers=headers).json()
+    if r.status_code == 200:
+        column_headers = r['resultSets'][1]['headers']
+        data = r['resultSets'][1]['rowSet']
+        df = pd.DataFrame(data, columns=column_headers)
+        df.drop(df.columns[[0,1,3,21,22,23,24,25,26,27,28]], axis=1, inplace=True)
+        return df
+    else:
+        return "STATUS CODE != 200"
+
+def create_df(r):
+    column_headers = r['resultSets'][0]['headers']
+    data = r['resultSets'][0]['rowSet']
+    return pd.DataFrame(data, columns=column_headers)    
+
+def get_standings(season='2019-20'): # season needs to be current automatically
+    url = Urls.leaguestandings(season)
+    headers = Urls.headers()
+    r = requests.get(url=url, headers=headers).json()
+    if r.status_code == 200:
+        league = create_df(r)
+        drop_range = range(20, 81)
+        league.drop(league.columns[drop_range], axis=1, inplace=True)
+        league.drop(league.columns[[0,1,2,7,12,13,15]], axis=1, inplace=True)
+        east = league[league['Conference'] == 'East'].reset_index()
+        west = league[league['Conference'] == 'West'].reset_index()
+        return {'league': league, 'east': east, 'west': west}
+    else:
+        return "STATUS CODE != 200"
+
+## Args: 
+# PlayerScope = ^(All+Players)|(Rookies)$
+ # Season needs to be current automatically
+# StatCategory = (Points)|(Rebounds)|(Assists)|(Defense)|(Clutch)|(Efficiency)|
+def leaders(scope='All+Players', season='2019-20', category='Points'):  
+    url = Urls.leaders(scope, season, category)
+    headers = Urls.headers()
+    r = requests.get(url=url, headers=headers).json()
+    if r.status_code == 200:
+        df = create_df(r)
+        drop_dict = {
+            'Points': [1,3,5,10,11,12],
+            'Rebounds': [1,3,5,9,10,11],
+            'Assists': [1,3,5,8,9,10],
+            'Defense': [1,3,5,9,10,11,12],
+            'Clutch': [1,3,5,11,12],
+            'Efficiency': [1,3,5,6,7]
+        }
+        df.drop(df.columns[drop_dict[category]], axis=1, inplace=True)
+        return df
+    else:
+        return "STATUS CODE != 200"
---
+++ lackey/actions/sports/soccer.py
@@ -0,0 +1,104 @@
+import requests
+import json
+
+import pandas as pd
+
+import Soccer_Urls from sport_urls as Urls
+
+## HIGHLIGHT EMBEDDER?
+##'https://www.scorebat.com/video-api/'
+
+# https://www.football-data.org/documentation/quickstart
+
+def get_comp_info(comp='PL'):
+    """
+    get general competition info for 
+    macthday scoreboard and UI
+    """
+    urls = Urls.urls_('matches', comp)
+    headers = Urls.headers()
+    r = requests.get(url=url, headers=headers).json()
+    current_matchday = r['matches'][0]['season']['currentMatchday']
+    stage = r['matches'][0]['stage']
+    comp_info = r['competition']
+    return {
+        'name': comp_info['name'],
+        'code': comp_info['code'],
+        'matchday': current_matchday,
+        'stage': stage,
+        'pulled': comp_info['lastUpdated']
+    }
+
+def fixture_list(matchday, comp='PL'):
+    """
+    get matches and match details
+    for specificied matchday
+    """
+    url = Urls.urls_('matches', comp) + f'?matchday={matchday}'
+    headers = Urls.headers()
+    r = requests.get(url=url, headers=headers).json()
+    
+    final_json = []
+    for each in r['matches']:
+        obj = {
+            'home_team':  each['homeTeam']['name'],
+            'away_team': each['awayTeam']['name'],
+            'status': each['status'],
+            'winner': each['score']['winner'],
+            'duration': each['score']['duration'],
+            'fullTime': each['score']['fullTime'],
+            'halfTime': each['score']['halfTime'],
+            'extraTime': each['score']['extraTime'],
+            'penalties': each['score']['penalties'],
+        }
+        final_json.append(obj)
+    return final_json
+
+def get_PL_table():
+    """
+    returns table for PL
+    """
+    url = Urls.urls_('table', "PL")
+    headers = Urls.headers()
+    r = requests.get(url=url, headers=headers).json()
+    table = r['standings'][0]['table']
+
+    df_data = []
+    for each in table:
+        obj = {
+            'position': each['position'],
+            'team': each['team']['name'],
+            'crest': each['team']['crestUrl'],
+            'playedGames': each['playedGames'],
+            'won': each['won'],
+            'draw': each['draw'],
+            'lost': each['lost'],
+            'points': each['points'],
+            'goalsFor': each['goalsFor'],
+            'goalsAgainst': each['goalsAgainst'],
+            'goalDifference': each['goalDifference'],
+        }
+        df_data.append(obj)
+
+    return pd.DataFrame(df_data)
+
+def get_top_scorers(comp='PL'):
+    """
+    returns table of comp top scorers
+    """
+    url = Urls.urls_('scorers', comp)
+    headers = Urls.headers()
+    r = requests.get(url=url, headers=headers).json()
+    table = r['scorers']
+
+    df_data = []
+    for each in table:
+        obj = {
+            'name': each['player']['name'],
+            'nationality': each['player']['nationality'],
+            'team': each['team']['name'],
+            'numberOfGoals': each['numberOfGoals'],
+            }
+        df_data.append(obj)
+
+    return pd.DataFrame(df_data)
