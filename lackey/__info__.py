e6c935277220f1f223028a6f048f6963523d2b9b
Wilson MacLeod <wilsonmacleod@gmail.com>
2020-06-15 13:01:52 -0700

    production and exe builder/installer work

--- lackey/__info__.py
+++ lackey/__info__.py
@@ -4,6 +4,9 @@ APP_NAME = "Lackey"
 APP_NAME_NO_SPACE = APP_NAME.replace(' ', '')
 APP_VERSION = '1.00'
 APP_DESCRIPTION = "Lower my Chrome tab count, Flask-based desktop application with React frontend"
+COMPANY_NAME = "MacLeod"
+WEBSITE = "https://wilsonmacleod.com/"
+
 
 NBA_SEASON = "2019-20" ##
 API_KEYS = {
---
+++ lackey/frontend/build/precache-manifest.fe680537fc03ff565f5c4f04bcb25b8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e51dc5721b450ce768e880a32717659",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e170d84ad03e8de3ff9",
+    "url": "/static/css/main.881a3f1d.chunk.css"
+  },
+  {
+    "revision": "e210356b7e6b8606ccc5",
+    "url": "/static/js/2.861be792.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.861be792.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e170d84ad03e8de3ff9",
+    "url": "/static/js/main.8b5ee54c.chunk.js"
+  },
+  {
+    "revision": "6b8b64c3543d6512e8be",
+    "url": "/static/js/runtime-main.ac92e1c8.js"
+  }
+]);
